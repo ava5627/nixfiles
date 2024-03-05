2570d14c500ef1939aa943054a638fd57f262a70
ava5627 <avasharris1@gmail.com>
2024-03-05 00:25:21 -0600

    firefox

--- modules/desktop/firefox.nix
+++ modules/desktop/firefox.nix
@@ -1,7 +1,11 @@
 { config, lib, pkgs, ... }:
 with lib;
 with lib.my;
-let cfg = config.modules.desktop.firefox;
+let
+    cfg = config.modules.desktop.firefox;
+    cfg_profile = config.home.programs.firefox.profiles.${config.user.name};
+    extension_name = "yaru_orange@ava.xpi";
+    extension_path = ".mozilla/firefox/${cfg_profile.name}/extensions/${extension_name}";
 in
 {
     options.modules.desktop.firefox = {
@@ -17,7 +21,7 @@ in
             profiles.${config.user.name} = {
                 name = "${config.user.name}";
                 isDefault = true;
-                userChrome = builtins.readFile "${config.dotfiles.config}/userChrome.css";
+                userChrome = builtins.readFile "${config.dotfiles.config}/firefox/userChrome.css";
                 settings = {
                     # Tell Firefox to sync the toolbar layout
                     "services.sync.prefs.sync.browser.uiCustomization.state" = true;
@@ -31,5 +35,6 @@ in
                 };
             };
         };
+        home.home.file.${extension_path}.source = "${config.dotfiles.config}/firefox/${extension_name}";
     };
 }
