299d3a13eb156ea71e0479a18d59b083407eebe1
ava5627 <avasharris1@gmail.com>
2024-03-08 17:00:19 -0600

    Donnager NixOS 6: xorg conf

--- hosts/donnager/default.nix
+++ hosts/donnager/default.nix
@@ -6,4 +6,6 @@
     modules.theme.active = "Tokyo Night";
     modules.hardware.nvidia.enable = true;
     boot.loader.systemd-boot.enable = true;
+
+    environment.etc."X11/xorg.conf".source = ./xorg.conf;
 }
