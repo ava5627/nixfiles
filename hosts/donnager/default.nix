1316551bba4b8c909b71f7945854684bb4ad626e
ava5627 <avasharris1@gmail.com>
2024-03-08 18:38:56 -0600

    Donnager NixOS 9: aaaaaaaaaa

--- hosts/donnager/default.nix
+++ hosts/donnager/default.nix
@@ -8,10 +8,21 @@
     hardware.nvidia.forceFullCompositionPipeline = true;
     boot.loader.systemd-boot.enable = true;
 
-    services.xserver.xrandrHeads = [
-        "DP-2"
-        { output = "DP-4"; primary = true; }
-        "DP-0"
-    ];
+    services.xserver = {
+        monitorSection = ''
+            VendorName     "Unknown"
+            ModelName      "DELL E2422H"
+            HorizSync       30.0 - 83.0
+            VertRefresh     50.0 - 75.0
+            Option         "DPMS"
+        '';
+        screenSection = ''
+            Option "metamodes" "DP-0: nvidia-auto-select +3840+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}, DP-4: nvidia-auto-select +1920+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}, DP-2: nvidia-auto-select +0+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On};
+            Option "MultiGPU" "Off"
+            Option "BaseMosaic" "off"
+            Option "Stereo" "0"
+            Option "nvidiaXineramaInfoOrder" "DFP-5"
+        '';
+    };
 
 }
