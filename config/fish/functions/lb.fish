9157cea07a1e98d1e186fb3833a0cfc07577239e
ava5627 <avasharris1@gmail.com>
2024-03-09 22:05:17 -0600

    Donnager NixOS 44: lb

--- config/fish/functions/lb.fish
+++ config/fish/functions/lb.fish
@@ -1,14 +1,12 @@
-function lb --description "ls if argument is a directory, bat otherwise"
-    for arg in $argv
-        if [ -e "$arg" ]
-            set args $args $arg
-        else
-            set opts $opts $arg
-        end
-    end
-    if [ -z "$args" ] || [ -d "$args" ]
-        lsd $argv
+for arg in $argv
+    if [ -e "$arg" ]
+        set args $args $arg
     else
-      bat $args
+        set opts $opts $arg
     end
+end
+if [ -z "$args" ] || [ -d "$args" ]
+    lsd $argv
+else
+  bat $args
 end
