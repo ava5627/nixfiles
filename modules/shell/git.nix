4069ebc86f67b6be01662ae2014334dd5aa1e409
ava5627 <avasharris1@gmail.com>
2024-03-11 00:12:25 -0500

    Donnager NixOS 64

--- modules/shell/git.nix
+++ modules/shell/git.nix
@@ -19,6 +19,7 @@ in {
         enable = true;
         userName = cfg.userName;
         userEmail = cfg.userEmail;
+        ignores = [ ".ropeproject" ".envrc" ".direnv" ];
         delta = {
           enable = true;
           options = {
