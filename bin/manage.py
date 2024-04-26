89e49960cfd6e03badc0e398dd217003b1c0df50
ava5627 <avasharris1@gmail.com>
2024-04-26 16:45:16 -0500

    Donnager NixOS 144

--- bin/manage.py
+++ bin/manage.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env nix-shell
 #! nix-shell -i python3 -p python3 python3Packages.argcomplete nvd git ripgrep python3Packages.rich
 # PYTHON_ARGCOMPLETE_OK
-# vim: ft=python
 import argparse
 import os
 import socket
