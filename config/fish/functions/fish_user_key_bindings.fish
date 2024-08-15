74ed91a24372fbf31e02f214b09c5d9040683227
ava5627 <avasharris1@gmail.com>
2024-08-15 17:49:52 -0500

    Donnager NixOS 239

--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -2,7 +2,7 @@ function fish_user_key_bindings
     bind \cq kill-whole-line
     bind \cr history-pager
     bind \ew backward-kill-bigword
-    bind \cj down-or-search
-    bind \ck up-or-search
+    bind \ej down-or-search
+    bind \ek up-or-search
     bind \er rm_rf
 end
