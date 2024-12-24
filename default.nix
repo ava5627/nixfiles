9f1068b09c62b91ec342e08e91a9b0bd2a4a11f3
ava5627 <avasharris1@gmail.com>
2024-12-24 14:04:44 -0600

    Donnager NixOS 265

--- default.nix
+++ default.nix
@@ -81,10 +81,12 @@ with lib.my; {
   ];
   fonts.packages = with pkgs; [
     noto-fonts
-    noto-fonts-cjk
+    noto-fonts-cjk-sans
     noto-fonts-emoji
     liberation_ttf
-    (nerdfonts.override {fonts = ["SourceCodePro" "Ubuntu" "UbuntuMono"];})
+    nerd-fonts.ubuntu-mono
+    nerd-fonts.ubuntu
+    nerd-fonts.sauce-code-pro
     source-code-pro
     # corefonts
   ];
