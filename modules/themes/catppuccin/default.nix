be1d9d0554b0eb2ff878ab6a6c9d174726f0b5df
ava5627 <avasharris1@gmail.com>
2025-02-05 21:29:36 -0600

    Donnager NixOS 282

--- modules/themes/catppuccin/default.nix
+++ modules/themes/catppuccin/default.nix
@@ -17,10 +17,10 @@ in {
         foreground = "#cdd6f4"; # text
         background = "#1e1e2e"; # base
         black = "#11111b"; # crust
-        blue = "#89b4fa";
-        gray = "#181825"; # mantle
+        blue = "#3d59a1";
+        gray = "#585b70";
         cyan = "#89dceb"; # sky
-        pink = "#cba6f7";
+        pink = "#cba6f7"; # mauve
         red = "#f38ba8";
       };
       qtile = {
@@ -29,30 +29,50 @@ in {
         active = cfg.colors.blue;
         inactive = cfg.colors.gray;
         current-group-background = cfg.colors.blue;
-        other-screen-group-background = cfg.colors.gray;
-        active-group-foreground = cfg.colors.cyan;
+        other-screen-group-background = "#181825";
+        active-group-foreground = cfg.colors.foreground;
         powerline-colors = [
           {
-            fg = cfg.colors.black;
-            bg = cfg.colors.cyan;
+            bg = "#f38ba8";
+            fg = "#181825";
           }
           {
-            fg = cfg.colors.cyan;
-            bg = cfg.colors.black;
+            bg = "#fab387";
+            fg = "#181825";
+          }
+          {
+            bg = "#f9e2af";
+            fg = "#181825";
+          }
+          {
+            bg = "#a6e3a1";
+            fg = "#181825";
+          }
+          {
+            bg = "#74c7ec";
+            fg = "#181825";
+          }
+          {
+            bg = "#45475a";
+            fg = "#cdd6f4";
+          }
+          {
+            bg = "#313244";
+            fg = "#cdd6f4";
           }
         ];
       };
       rofi = {
-        accent = cfg.colors.cyan;
+        accent = "#89b4fa";
         accent-alt = cfg.colors.pink;
-        background = cfg.colors.background;
-        background-light = cfg.colors.black;
+        background = cfg.colors.black;
+        background-light = cfg.colors.background;
         text-color = cfg.colors.foreground;
       };
       # source: https://github.com/folke/tokyonight.nvim/blob/main/extras/dunst/tokyonight_night.dunstrc
     };
     catppuccin = {
-      accent = "sky";
+      accent = "mauve";
       flavor = "mocha";
       sddm.enable = true;
       sddm.background = "${config.dotfiles.config}/camp_fire.jpg";
@@ -65,7 +85,7 @@ in {
       qt.style.name = "kvantum";
       qt.platformTheme.name = "kvantum";
       catppuccin = {
-        accent = "sky";
+        accent = "mauve";
         flavor = "mocha";
         bat.enable = true;
         btop.enable = true;
