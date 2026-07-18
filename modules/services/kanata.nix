e2610672effaea57ff267fdbd539d806eec7b790
ava5627 <avasharris1@gmail.com>
2026-07-17 20:38:03 -0600

    Donnager NixOS 397

--- modules/services/kanata.nix
+++ modules/services/kanata.nix
@@ -23,7 +23,7 @@ in {
             (defsrc
                 q w e r t
                 a s d f g
-                z x c v b
+                z x c v b lsft spc
                 mfwd mbck
             )
 
@@ -35,18 +35,18 @@ in {
             (deflayer default
                 q w e r t
                 a s d f g
-                z x c v b
+                z x c v b lsft spc
                 @mnum @mblf
             )
             (deflayer num
                 7 8 9 / _
-                4 5 6 - _
+                4 5 6 - _ bspc ret
                 _ 1 2 3 S-8
                 _ _
             )
             (deflayer left
                 o i u y _
-                l k j h _
+                l k j h _ _ _
                 _ _ m n p
                 _ _
             )
